c8ba0f780957fe89a9c0426cad41f100ece3c251
fildahananda <fildahananda@gmail.com>
2020-01-07 11:07:04 +0700

    Updates

---
+++ precache-manifest.4c06f26f1d7cf1c12d5440cee09b2ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83634202f5946e99c06df2bd6e0aca0",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "837976af094cdfbc56bc",
+    "url": "/react-monster-rolodex/static/css/main.42b4bb03.chunk.css"
+  },
+  {
+    "revision": "4ee796cfabedc5bd953d",
+    "url": "/react-monster-rolodex/static/js/2.cd5ddd3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster-rolodex/static/js/2.cd5ddd3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "837976af094cdfbc56bc",
+    "url": "/react-monster-rolodex/static/js/main.37960295.chunk.js"
+  },
+  {
+    "revision": "12ddb34c8361ecf0893b",
+    "url": "/react-monster-rolodex/static/js/runtime-main.355bb653.js"
+  }
+]);
